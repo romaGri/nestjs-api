f72be75489e5316cc9c041f2692bd2962f778f7f
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2020-08-05 12:55:28 +0300

    added user entity

--- demo-api/src/app.module.ts
+++ demo-api/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from "typeorm";
+import { User } from './user/entity/user.entity'
+import { rootCertificates } from 'tls';
 
 @Module({
-  imports: [UserModule, AuthModule],  
+  imports: [UserModule, AuthModule, 
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [User],
+    synchronize: true,
+  }),],  
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection){}
+}
+
+
